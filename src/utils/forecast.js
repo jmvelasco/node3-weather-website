de486ad0482bc1d3cb7796c316959d05ffe614fd
JManuel Velasco <jmvelasco@obs.tv>
2020-01-31 14:52:21 +0100

    Improve the forecast output string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The min/max temperature for today is ' + body.daily.data[0].temperatureLow + '/' + body.daily.data[0].temperatureHigh + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The min/max temperature for today is ' + body.daily.data[0].temperatureLow + '/' + body.daily.data[0].temperatureHigh + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
